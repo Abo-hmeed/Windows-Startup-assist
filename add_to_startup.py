87e8f87987c6aa156fa06979fc2f36c0f6cccf87
محمد طارق محمد نبهان سيد احمد <122485820+mohamedtarek132@users.noreply.github.com>
2023-10-14 00:45:50 +0300

    Update add_to_startup.py

--- add_to_startup.py
+++ add_to_startup.py
@@ -1,13 +1,22 @@
 from sys import argv
 from shutil import copy
 from winshell import startup
+from os import listdir
+
 
 def add_to_startup():
-    file = open('done.txt', 'r')
-    if (file.read()) == '0':
+    opened_befor = True
+    path = sys.argv[0].replace('\\open programs.py','')
+    if 'done.txt' not in os.listdir(path):
+        file = open('done.txt', 'x')
+    else:
+        file = open('done.txt', 'r')
+    if (file.read()) != '1':
         path = argv[0].replace('main.py','add_to_startup - Shortcut')
         copy(path, startup())
         file.close()
         file = open('done.txt', 'w')
         file.write('1')
+        opened_befor = False
     file.close()
+    return opened_befor
