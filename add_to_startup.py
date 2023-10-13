acdad1c476b9361832e0518938abbe44fa83be09
محمد طارق محمد نبهان سيد احمد <122485820+mohamedtarek132@users.noreply.github.com>
2023-10-14 00:32:18 +0300

    Update add_to_startup.py

--- add_to_startup.py
+++ add_to_startup.py
@@ -1,15 +1,14 @@
-import sys
-import os
-import shutil
-
+from sys import argv
+from shutil import copy
+from winshell import startup
 
 def add_to_startup():
     file = open('done.txt', 'r')
     print()
     if (file.read()) == '0':
-        path = sys.argv[0].replace('main.py','add_to_startup - Shortcut')
+        path = argv[0].replace('main.py','add_to_startup - Shortcut')
         print(path)
-        shutil.copy(path, r'C:\Users\hp\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup')
+        copy(path, startup())
         file.close()
         file = open('done.txt', 'w')
         file.write('1')
