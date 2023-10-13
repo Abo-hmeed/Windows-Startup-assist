99887b7a3859abd2d1c38b96b92614a42b9ca343
محمد طارق محمد نبهان سيد احمد <122485820+mohamedtarek132@users.noreply.github.com>
2023-10-14 01:27:11 +0300

    Update open programs.py
    added to get the file name from the pc

--- open programs.py
+++ open programs.py
@@ -1,12 +1,14 @@
 import os
-import sys
+from os import path as pth
+from sys import argv
 
 
 def open_programs():
-    path = sys.argv[0].replace('\\open programs.py','')
+    file_name = '\\' + pth.basename(argv[0])
+    path = argv[0].replace(file_name, '')
     if 'programs' not in os.listdir(path):
         os.mkdir('programs')
-    path = path + '\\programs'
+    path = path + '\\progras'
     if len(os.listdir(path)) == 0:
         os.startfile(path)
     else:
