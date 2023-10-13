07fa34c960eb1dfb01d55c335a1b19e62eced5a7
محمد طارق محمد نبهان سيد احمد <122485820+mohamedtarek132@users.noreply.github.com>
2023-10-14 01:13:48 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import add_to_startup as ats
+import greet_user as gs
+import mainMenu as mm
+import Menus as m
+import open_programs as op
+
+
+gs.greetUser(ats.add_to_startup())
+choice = mm.mainMenu()
